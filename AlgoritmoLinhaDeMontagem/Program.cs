4a4a644eacfe2dc7044cbee191cba69549a91546
Yan Paulo <yanpaulo@hotmail.com>
2016-09-05 19:27:16 -0300

    Implementado sistema para entrada de dados.

--- AlgoritmoLinhaDeMontagem/Program.cs
+++ AlgoritmoLinhaDeMontagem/Program.cs
@@ -1,10 +1,9 @@
-﻿using System;
+﻿using System.Linq;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static System.Math;
 using static System.Console;
+
 
 namespace AlgoritmoLinhaDeMontagem
 {
@@ -38,41 +37,35 @@ namespace AlgoritmoLinhaDeMontagem
     }
 
     #endregion
+
     class Program
     {
+        static readonly string ArquivoPadrao = "entrada.txt";
+
         static void Main(string[] args)
         {
-            //Criação declarativa (não-programática) das Linhas f1 e f2
-            var f1 = new LinhaProducao
+            List<LinhaProducao> fList;
+            if (args.Length > 0)
             {
-                Numero = 1,
-                TempoEntrada = 2,
-                TempoSaida = 3,
-                Estacoes = new List<Estacao>
-                {
-                    new Estacao(7, 2), new Estacao(9, 3), new Estacao(3, 1),
-                    new Estacao(4, 3), new Estacao(8, 4), new Estacao(4)
-                }
-            };
-            var f2 = new LinhaProducao
+                fList = LeArquivo(args[0]).ToList();
+            }
+            else if (File.Exists(ArquivoPadrao))
             {
-                Numero = 2,
-                TempoEntrada = 4,
-                TempoSaida = 2,
-                Estacoes = new List<Estacao>()
-                {
-                    new Estacao(8, 2), new Estacao(5, 1), new Estacao(6, 2),
-                    new Estacao(4, 2), new Estacao(5, 1), new Estacao(7)
-                }
-            };
-            var fList = new List<LinhaProducao> { f1, f2 };
+                fList = LeArquivo(ArquivoPadrao).ToList();
+            }
+            else
+            {
+                fList = EntradaPadrao().ToList();
+            }
+
             //Linq Query para atribuir a linha à qual cada estação pertence.
             fList.ForEach(f => f.Estacoes.ForEach(e => e.Linha = f));
 
-            //Já sabemos o que AlgoritmoDinamico(f1, f2) retorna.
+            //Já sabemos o que AlgoritmoDinamico(fList[0], fList[1]) retorna.
             ///Não sabe? Passe o mouse em cima! :D
-            var path = AlgoritmoDinamico(f1, f2).ToList();
-            
+            var path = AlgoritmoDinamico(fList[0], fList[1]).ToList();
+
+            //Imprime a saída detalhada
             path.Select((e, i) =>
                 new
                 {
@@ -84,16 +77,18 @@ namespace AlgoritmoLinhaDeMontagem
 
             WriteLine();
 
+            //Imprime a saída comparativa
             foreach (var f in fList)
             {
                 f.Estacoes.ForEach(e => { Write($"{e.TempoRealProducao}{(path.Contains(e) ? "*" : "")}\t"); });
                 WriteLine();
             }
-                
+
 
             ReadKey();
         }
 
+        #region Algoritmo
         /// <summary>
         /// Calcula o TRP (Tempo Real de Produção) para as estações da linha f1 e f2.
         /// </summary>
@@ -124,7 +119,7 @@ namespace AlgoritmoLinhaDeMontagem
                 v2 = _e2.TempoRealProducao.Value + _e2.TempoTransferencia.GetValueOrDefault(0) + e1.TempoProducao;
 
                 e1.TempoRealProducao = Min(v1, v2);
-                
+
                 //Note que agora v1 é referente ao tempo de _e2, e não de _e1
                 v1 = _e2.TempoRealProducao.Value + e2.TempoProducao;
                 v2 = _e1.TempoRealProducao.Value + _e1.TempoTransferencia.GetValueOrDefault(0) + e2.TempoProducao;
@@ -144,5 +139,63 @@ namespace AlgoritmoLinhaDeMontagem
             path[path.Count - 1] = e1.TempoRealProducao <= e2.TempoRealProducao ? e1 : e2;
             return path.ToArray();
         }
+        #endregion
+
+        #region Entrada de Dados
+        static LinhaProducao[] LeArquivo(string path)
+        {
+            var content = File.ReadAllLines(path).Skip(1)
+            .Select((l, i) =>
+            {
+                var _l = l.Split(new[] { ',' }).Select(lineItem => lineItem.Trim());
+                var f = new LinhaProducao
+                {
+                    Numero = i + 1,
+                    TempoEntrada = int.Parse(_l.First()),
+                    TempoSaida = int.Parse(_l.Last()),
+                    Estacoes = _l.Skip(1).Take(_l.Count() - 2).Select(pair =>
+                    {
+                        var _pair = pair.Split(new[] { '-' }).Select(pairItem => pairItem.Trim()).ToArray();
+                        return new Estacao(int.Parse(_pair[0]), tempoTransferencia: _pair.Length > 1 ? int.Parse(_pair[1]) as int? : null);
+                    }).ToList()
+                };
+                return f;
+            });
+
+            return content.ToArray();
+        }
+
+        static LinhaProducao[] EntradaPadrao()
+        {
+            return new[]
+            { 
+                //Criação declarativa (não-programática) das Linhas f1 e f2
+                new LinhaProducao
+                {
+                    Numero = 1,
+                    TempoEntrada = 2,
+                    TempoSaida = 3,
+                    Estacoes = new List<Estacao>
+                    {
+                        new Estacao(7, 2), new Estacao(9, 3), new Estacao(3, 1),
+                        new Estacao(4, 3), new Estacao(8, 4), new Estacao(4)
+                    }
+                }
+                ,
+                new LinhaProducao
+                {
+                    Numero = 2,
+                    TempoEntrada = 4,
+                    TempoSaida = 2,
+                    Estacoes = new List<Estacao>()
+                    {
+                        new Estacao(8, 2), new Estacao(5, 1), new Estacao(6, 2),
+                        new Estacao(4, 2), new Estacao(5, 1), new Estacao(7)
+                    }
+                }
+            };
+        }
+        #endregion
+
     }
 }
